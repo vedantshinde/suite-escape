9a5b210c12550088a0619c8b016a0097d497acb4
Vedant Shinde <vedantshinde101@gmail.com>
2020-12-30 14:54:39 +0530

    Initial commit

---
+++ src/components/aboutUs.js
@@ -0,0 +1,40 @@
+export default function AboutUs() {
+  return (
+    <main>
+      <section class="section-about">
+        <div class="u-center-text u-margin-bottom-big">
+          <h2 class="heading-secondary">
+            Exciting tours for adventurous people
+          </h2>
+        </div>
+
+        <div class="row">
+          <div class="col-1-of-2">
+            <h3 class="heading-tertiary u-margin-bottom-small">
+              You're going to fall in love with the nature
+            </h3>
+            <p class="paragraph">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta
+              eos repellendus natus ex maiores assumenda, voluptatum eius
+              obcaecati nobis asperiores dolorem quia vero laboriosam, iure
+              tempore accusantium? Voluptatum, magnam laboriosam.
+            </p>
+
+            <h3 class="heading-tertiary u-margin-bottom-small">
+              Live like you never have before
+            </h3>
+            <p class="paragraph">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis,
+              saepe voluptatibus velit repellat non blanditiis amet.
+            </p>
+
+            <a href="#" class="btn-txt">
+              Learn more &rarr;
+            </a>
+          </div>
+          <div class="col-1-of-2">Image Content</div>
+        </div>
+      </section>
+    </main>
+  );
+}
---
+++ src/components/header.js
@@ -0,0 +1,25 @@
+import logoWhite from "../img/logo-white.png";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header__logo-box">
+        <img src={logoWhite} alt="Logo" className="header__logo" />
+      </div>
+
+      <div className="header__text-box">
+        <h1 className="heading-primary">
+          <span className="heading-primary--main">Nature</span>
+          <span className="heading-primary--sub">is where you belong</span>
+        </h1>
+
+        <a href="#" className="btn btn--white btn--animated">
+          Discover our tours
+        </a>
+        <a href="#" className="btn btn--white btn--infinity btn--animated">
+          About Us
+        </a>
+      </div>
+    </header>
+  );
+}
